59a87bd5c67ceeab93f16e3a13d8f2e60c417ccf
deiby33 <deybimerkado@gmail.com>
2022-10-27 08:37:59 -0500

    first commit

---
+++ suma.php
@@ -0,0 +1,5 @@
+<?php
+    $num1 = $_POST["Num1"];
+    $num2 = $_POST["Num2"];
+    echo $num1 + $num2;
+?>
---
+++ variable.php
@@ -0,0 +1,13 @@
+<?php
+//declaracion de variables
+    $numero = 5;
+    $nume2 = 2;
+    $suma = $numero + $nume2;
+    //$cadena = "yo soy una cadena de texto";
+    
+    echo $suma;
+   /*  echo "<br>";
+    echo $cadena;
+    echo "<br>";
+    echo $numero." ".$cadena; */
+?>
