c97b8ed8112a15a65006da430ef3b3ff0d962978
deiby2506 <djoyola2@misena.edu.co>
2022-11-09 09:57:24 -0500

    canbio

---
+++ tabla-2.php
@@ -0,0 +1,7 @@
+<?php
+    for ($tabla1=1; $tabla1<=10 ; $tabla1++){ 
+        for ($tabla2=1; $tabla2 <=10 ; $tabla2++) { 
+             echo $tabla1." x " .$tabla2. " = ".$tabla2 * $tabla1." <br> ";
+    }
+    }
+?>
